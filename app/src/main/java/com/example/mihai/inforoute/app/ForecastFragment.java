89183b3f2ca4ce99b61a2072f2a3f114b53d7e71
comihai <coca.mihai5@gmail.com>
2015-05-12 17:42:48 +0300

    change UI details

--- app/src/main/java/com/example/mihai/inforoute/app/ForecastFragment.java
+++ app/src/main/java/com/example/mihai/inforoute/app/ForecastFragment.java
@@ -221,6 +221,29 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     public static String formatTemperature(Context context, double temperature) {
         return context.getString(R.string.format_temperature, temperature);
     }
+    public static String getFormattedWind(Context context, float windSpeed, float degrees) {
+        int windFormat = R.string.format_wind_kmh;
+
+        String direction = "Unknown";
+        if (degrees >= 337.5 || degrees < 22.5) {
+            direction = "N";
+        } else if (degrees >= 22.5 && degrees < 67.5) {
+            direction = "NE";
+        } else if (degrees >= 67.5 && degrees < 112.5) {
+            direction = "E";
+        } else if (degrees >= 112.5 && degrees < 157.5) {
+            direction = "SE";
+        } else if (degrees >= 157.5 && degrees < 202.5) {
+            direction = "S";
+        } else if (degrees >= 202.5 && degrees < 247.5) {
+            direction = "SW";
+        } else if (degrees >= 247.5 && degrees < 292.5) {
+            direction = "W";
+        } else if (degrees >= 292.5 && degrees < 337.5) {
+            direction = "NW";
+        }
+        return String.format(context.getString(windFormat), windSpeed, direction);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -353,10 +376,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        //if(loader.getId() == 0)
             mForecastAdapter.swapCursor(null);
-        //else
-        //    mRouteAdapter.swapCursor(null);
+            mRouteAdapter.swapCursor(null);
     }
 
 
